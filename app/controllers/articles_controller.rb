9d27829551899eeb77f0fc0cca569462b0a37f39
Steven <stevenchoy0915@gmail.com>
2021-08-23 20:51:12 +0800

    make it better

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,60 @@
+class ArticlesController < ApplicationController
+    
+  def index #index.html.erb
+    @articles = Article.all 
+
+  end 
+
+  def show
+    @article = Article.find(params[:id])
+  end 
+
+  def find_name
+    @article = Article.find_by(name: params[:name])
+  end 
+
+  def new
+    @article = Article.new
+  end
+ 
+  def create
+    @article = Article.new(article_params)
+
+    # @article.name = params[:name] 
+    # @article.body = params[:body] 
+
+    if @article.save
+    redirect_to articles_path
+    else 
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to articles_path
+    else 
+      render :edit
+    end
+  end 
+
+  def delete
+    @article = Article.find(params[:id])
+    if @article.destroy
+      redirect_to articles_path
+    else 
+      render :index
+    end
+  end 
+
+  private
+
+  def article_params
+    params.require(:article).permit(:name, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   resources :form_newspapers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #routes are plural
+  # get {any name} => {controller}
+  get '/articles' => 'articles#index'
+  get '/articles/new' => 'articles#new'
+  get '/articles/name/:name' => 'articles#find_name'
+  get '/articles/:id' => 'articles#show', as: 'article_show'
+  delete '/articles/:id' => 'articles#delete', as: 'article_delete'
+  get '/articles/:id/edit' => 'articles#edit', as: 'article_edit'
+  put '/articles/:id' => 'articles#update', as: 'article_update'
+  post '/articles' => 'articles#create', as: 'article_create'
+
 end
